9e9eabe9615a99f198180c23f7de27419b6deb8f
Caike <gcmenezes1@gmail.com>
2023-09-05 08:32:15 -0400

    Start using eslint and prettier; styles update

--- App.js
+++ App.js
@@ -1,34 +1,38 @@
-import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import React from 'react';
-import { StatusBar, StyleSheet, SafeAreaView, Text, View, Platform } from 'react-native';
-
-//const isAndroid = Platform.OS === "android";
-//console.log(StatusBar.currentHeight);
+import { StatusBar as ExpoStatusBar} from "expo-status-bar";
+import React from "react";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+      <SafeAreaView style={ styles.container }>
 
-        <View style={{ padding: 16, backgroundColor: "yellow" }}>
+        <View style={ styles.search }>
           <Text>Search</Text>
         </View>
 
-        <View style={{ flex: 1, padding: 16, backgroundColor: "red" }}>
+        <View style={ styles.list }>
           <Text>List</Text>
         </View>
 
       </SafeAreaView>
-      <ExpoStatusBar style="auto" />
+      <ExpoStatusBar style= "auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, 
+    marginTop: StatusBar.currentHeight
   },
+  search: {
+    padding: 16, 
+    backgroundColor: "yellow"
+  },
+  list: {
+    flex: 1, 
+    padding: 16, 
+    backgroundColor: "red"
+  }
 });
